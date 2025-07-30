57b3c1f63bd817fa82561b454a48ecbfc79d78da
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2025-07-30 22:21:36 +0530

    Create CircularLLTraversal.java

---
+++ LinkedList/CircularLLTraversal.java
@@ -0,0 +1,10 @@
+class Solution {
+    void printList(Node head) {
+        Node temp = head;
+        while(temp.next!=head){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.print(temp.data+" ");
+    }
+}
